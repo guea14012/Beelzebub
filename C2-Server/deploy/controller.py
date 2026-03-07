63b2d7e964507296ffa020ca6bb559238a6d39b4
guea14012 <gor.kua145@gmail.com>
2026-03-07 14:22:10 +0000

    Change contract address to placeholder
    Updated contract address placeholder in controller.py

--- C2-Server/deploy/controller.py
+++ C2-Server/deploy/controller.py
@@ -12,7 +12,7 @@ w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 with open("abi.json") as f:
     abi = json.load(f)
 
-contract_address = "0x0C12Fd4606cb8485b1a3EC363d2E432D570F9d69"
+contract_address = "PUT_CONTRACT_ADDRESS"
 
 contract = w3.eth.contract(address=contract_address, abi=abi)
 
@@ -28,3 +28,4 @@ encrypted = cipher.encrypt(payload.encode())
 tx = contract.functions.setCommand(encrypted.decode()).transact({
     "from": account
 })
+
